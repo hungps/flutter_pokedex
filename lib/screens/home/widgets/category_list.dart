de7d5f3a6ce634682247b0901a73b2641176bc8e
Hung Pham <hungps.work@gmail.com>
2020-07-14 16:49:09 +0700

    resolve #12

--- lib/screens/home/widgets/category_list.dart
+++ lib/screens/home/widgets/category_list.dart
@@ -6,16 +6,18 @@ import '../../../widgets/poke_category_card.dart';
 class CategoryList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return GridView.builder(
       physics: NeverScrollableScrollPhysics(),
       shrinkWrap: true,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        childAspectRatio: 2.44,
+        childAspectRatio: 2.58,
         crossAxisSpacing: 10,
         mainAxisSpacing: 12,
       ),
-      padding: EdgeInsets.only(left: 28, right: 28, bottom: 58),
+      padding: EdgeInsets.only(left: 28, right: 28, bottom: 30),
       itemCount: categories.length,
       itemBuilder: (context, index) => PokeCategoryCard(
         categories[index],
