ef2aa1d5b223fc12df3970def2f07fef6201940d
Jayant <jayantsogikar@gmail.com>
2020-11-10 13:09:17 +0530

    Added comments

--- lib/ui/screens/types/type_card.dart
+++ lib/ui/screens/types/type_card.dart
@@ -6,59 +6,6 @@ import 'package:pokedex/ui/screens/types/type_entities/type_indices.dart';
 
 import 'bold_texts.dart';
 
-// A class that is responsible for the pokeball kind of grids that contain types
-class CircularContainer extends StatelessWidget {
-  const CircularContainer({
-    Key key,
-    @required this.width,
-    @required this.index,
-  }) : super(key: key);
-
-  final double width;
-  final int index;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-          child: Container(
-            decoration: BoxDecoration(
-                shape: BoxShape.circle, border: Border.all(color: Colors.black87, width: 2.5)),
-          ),
-        ),
-        Align(
-            child: Container(
-          height: 5,
-          color: Colors.white,
-        )),
-        Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: width / 7,
-            decoration: BoxDecoration(
-                color: Colors.white70,
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.black87, width: 2.5)),
-          ),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: TypeDisplayContainer(
-            index: index,
-            path: "name",
-            value: null,
-            width: null,
-            j: null,
-            height: 30,
-            typeList: [],
-          ),
-        ),
-      ],
-    );
-  }
-}
-
 // The Class that is responsible for the type images
 class TypeDisplayContainer extends StatelessWidget {
   const TypeDisplayContainer(
