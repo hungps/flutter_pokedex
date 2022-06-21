345381db504e8e73d6ebdca0d762fb2ce8b6a225
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-06-21 23:48:12 +0530

    Appbar bug fix

--- lib/ui/widgets/main_app_bar.dart
+++ lib/ui/widgets/main_app_bar.dart
@@ -58,7 +58,7 @@ class MainSliverAppBar extends SliverAppBar {
               final dx = startX + endX - endX * percent;
 
               return Container(
-                color: Colors.white.withOpacity(0.8 - percent * 0.8),
+                color: Theme.of(context).backgroundColor.withOpacity(0.8 - percent * 0.8),
                 child: Stack(
                   fit: StackFit.expand,
                   children: <Widget>[
