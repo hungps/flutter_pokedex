c8202d5e6617311005cb844ca8d65ec0bc0bd5b0
Agustin <59627641+AgustinDipolito@users.noreply.github.com>
2022-11-01 16:51:58 -0300

    Update size.dart
    Could this be a clearly format ?

--- lib/utils/size.dart
+++ lib/utils/size.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-Size getTextSize(BuildContext context, String text, TextStyle style) => (TextPainter(
-        text: TextSpan(
-          text: text,
-          style: style,
-        ),
-        maxLines: 1,
-        textScaleFactor: MediaQuery.of(context).textScaleFactor,
-        textDirection: TextDirection.ltr)
-      ..layout())
-    .size;
+Size getTextSize(BuildContext context, String text, TextStyle style) {
+    final textPainter = TextPainter(
+      text: TextSpan(
+        text: text,
+        style: style,
+      ),
+      maxLines: 1,
+      textScaleFactor: MediaQuery.of(context).textScaleFactor,
+      textDirection: TextDirection.ltr,
+    )..layout();
+    return textPainter.size;
+  }
